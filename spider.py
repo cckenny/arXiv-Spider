c0ef3a0f894c1101f8086aaa5255cb13b97b9f64
cckenny <37532227+cckenny@users.noreply.github.com>
2019-04-09 16:26:14 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,6 +1,5 @@
 import os
-import urllib2
-
+import urllib.request
 
 class Spider:
     base_url = ''
@@ -15,16 +14,16 @@ class Spider:
     # to get the web page, default using relative path
     def get_page(self, page_url, is_complete=False):
         url = page_url if is_complete else self.base_url + page_url
-        request = urllib2.Request(url)
-        response = urllib2.urlopen(request)
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
         return response.read()
 
     # to downloadd the file, default using relative path
     # save_path should contain the file's name
     def get_file(self, file_name, file_url, is_complete=False):
         url = file_url if is_complete else self.base_url + file_url
-        request = urllib2.Request(url)
-        response = urllib2.urlopen(request)
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
         data = response.read()
         save_path = self.save_dir + file_name
         fd = open(save_path, 'wb')
